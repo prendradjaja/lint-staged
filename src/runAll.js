c9fd86cb3c1b6269df2689e31a36a83f4bdd8ab7
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2019-10-21 16:18:15 -0700

    write to real file location

--- src/runAll.js
+++ src/runAll.js
@@ -127,7 +127,7 @@ https://github.com/okonet/lint-staged#using-js-functions-to-customize-linter-com
           {
             title: 'You can start writing a commit message while you wait',
             task: () => input('Commit message', {
-              done: value => writeFileSync('/Users/pandu/temp-example-lint-staged', value)
+              done: value => writeFileSync(gitDir + '/.git/temp-commit-message', value)
             })
           }
         ], { ...listrOptions, concurrent: true, exitOnError: false })
